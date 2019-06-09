fa492d29fdb275457c8dfa58abdfc207e8b30584
Aldo Costa <aldocostasantos@hotmail.com>
2019-06-09 17:28:44 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}  from 'react';
+import './App.css'
+import MultiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { AppBar, Drawer, List, ListItem} from 'material-ui';
+import {BrowserRouter, Route, NavLink } from 'react-router-dom'; 
+import Notes from './Pages/Notes';
+import Diario from './Pages/Diario';
+import Todo from './Pages/Todo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+class App extends Component {
+  constructor(){
+  super();
+   this.state = {
+     draweVisible:false
+   } 
+   //this._tooGle.bind(this);
+  }
+
+_tooGle(){
+  this.setState({draweVisible: !this.state.draweVisible});
+}
+
+  render () {return (
+    <BrowserRouter>
+      <div>
+        <MultiThemeProvider>
+          <div>
+            <AppBar onLeftIconButtonClick={()=> this._tooGle()}/>                    
+            <Route path="/" component={Notes}/>
+            <Route path="/diario" component={Diario}/>
+            <Route path="/todo" component={Todo}/>
+            <Drawer open={this.state.draweVisible} docked={false} onRequestChange={() => this._tooGle()}>          
+              <List>
+                <ListItem>
+                  <NavLink to="/">Notas</NavLink>
+                </ListItem>
+                <ListItem>
+                  <NavLink to="/diario">Diário</NavLink>
+                </ListItem>
+                <ListItem>
+                  <NavLink to="/todo">Todo</NavLink>
+                </ListItem>                
+              </List>
+            </Drawer>
+          </div>
+        </MultiThemeProvider>
+      </div>
+    </BrowserRouter>
   );
+  }
 }
 
 export default App;
---
+++ src/Pages/Diario.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Diario extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        return (
+            <div>
+                Diario
+            </div>
+        )
+    }
+}
